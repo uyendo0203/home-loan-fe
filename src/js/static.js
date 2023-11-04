7a84b28984f7892bb4f4b6322c8b2eb3da31623f
Uyen Do <uyendo.0203@gmail.com>
2023-11-04 16:39:14 +0700

    add arrow for people say section

--- src/js/static.js
+++ src/js/static.js
@@ -146,6 +146,10 @@ $(document).ready(function () {
       type: 'bullets',
       clickable: true,
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
     // And if we need scrollbar
     // scrollbar: {
